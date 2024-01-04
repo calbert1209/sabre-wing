15c71f9e526bee4dd39409363c444fe18550a08c
Christian Albert <calbert1209@gmail.com>
2024-01-04 11:31:29 +0900

    remove other extras from home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,5 @@
-import preactLogo from "../../assets/preact.svg";
 import "./style.css";
 
 export function Home() {
-  return (
-    <div class="home">
-      <a href="https://preactjs.com" target="_blank">
-        <img src={preactLogo} alt="Preact logo" height="160" width="160" />
-      </a>
-      <h1>Get Started building Vite-powered Preact Apps </h1>
-    </div>
-  );
+  return <h1>SabreWing</h1>;
 }
