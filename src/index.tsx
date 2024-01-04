52e3582f7e17f47c071dda430df79ec737a22ea4
Christian Albert <calbert1209@gmail.com>
2024-01-04 11:37:28 +0900

    add settings page

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import { render } from "preact";
 import { LocationProvider, Router, Route } from "preact-iso";
 
-import { Home } from "./pages/Home/index.jsx";
-import { NotFound } from "./pages/_404.jsx";
+import { NotFound } from "./pages/_404";
 import "./style.css";
+import { Settings } from "./pages/Settings/index";
 
 export function App() {
   return (
     <LocationProvider>
       <main>
         <Router>
-          <Route path="/" component={Home} />
+          <Route path="/" component={Settings} />
           <Route default component={NotFound} />
         </Router>
       </main>
