fc6052d956675ab4c27327eeccbbcb9db0455f9f
Christian Albert <calbert1209@gmail.com>
2024-01-04 12:21:37 +0900

    add rough layout for settings page

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,3 +1,37 @@
+import { NumberInput } from "@/components/NumberInput";
+
+function StepSettings({ stepIndex }: { stepIndex: number }) {
+  return (
+    <fieldset>
+      <NumberInput name="stepTime" label="t" initialValue={30} readOnly />
+      <NumberInput name="stepWater" label="w" />
+    </fieldset>
+  );
+}
+
+function Summary() {
+  return (
+    <fieldset>
+      <NumberInput name="doseInput" label="d" />
+      <NumberInput
+        name="totalWaterInput"
+        label="total w"
+        initialValue={250}
+        readOnly
+      />
+    </fieldset>
+  );
+}
+
 export function Settings() {
-  return <h1>Settings</h1>;
+  return (
+    <section>
+      <form>
+        <Summary />
+        {[0, 1, 2, 3, 4].map((n) => (
+          <StepSettings key={`item-${n}`} stepIndex={n} />
+        ))}
+      </form>
+    </section>
+  );
 }
