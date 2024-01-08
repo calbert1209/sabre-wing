96c4f05f8bb982092b696d8268952f50e54db24f
Christian Albert <calbert1209@gmail.com>
2024-01-08 16:03:16 +0900

    use pacer to drive UI updates

--- src/pages/Pour/index.tsx
+++ src/pages/Pour/index.tsx
@@ -2,31 +2,45 @@ import { Routes } from "@/constants/routes";
 import { useRecipeStateContext } from "@/providers/RecipeStateProvider";
 import { ButtonOnClickHandler } from "@/types";
 import { useLocation } from "preact-iso";
-import { useCallback } from "preact/hooks";
+import { useCallback, useEffect, useMemo } from "preact/hooks";
+import { Pacer, PacerStep } from "./pacer";
+import { useComputed } from "@preact/signals";
 
 export function Pour() {
   const { route } = useLocation();
 
   const recipe = useRecipeStateContext();
 
+  const pacerSteps = useComputed(() => {
+    return recipe.steps.signal.value.map(
+      ({ time, water }) => new PacerStep(time, water)
+    );
+  });
+  const pacer = useMemo(() => {
+    return new Pacer(pacerSteps.value, 1000);
+  }, [pacerSteps.value]);
+
+  useEffect(() => {
+    return () => {
+      pacer.stop();
+    };
+  }, []);
+
   const handleOnClickSettings = useCallback<ButtonOnClickHandler>(
     () => route(Routes.root),
     []
   );
 
-  const handleOnStart = useCallback<ButtonOnClickHandler>(
-    () => console.log("start", recipe),
-    []
-  );
-  const handleOnPause = useCallback<ButtonOnClickHandler>(
-    () => console.log("pause", recipe),
-    []
-  );
+  const handleOnStart = useCallback<ButtonOnClickHandler>(() => {
+    pacer.start();
+  }, []);
+  const handleOnPause = useCallback<ButtonOnClickHandler>(() => {
+    pacer.stop();
+  }, []);
 
-  const handleOnReset = useCallback<ButtonOnClickHandler>(
-    () => console.log("reset", recipe),
-    []
-  );
+  const handleOnReset = useCallback<ButtonOnClickHandler>(() => {
+    pacer.reset();
+  }, []);
 
   return (
     <section>
@@ -35,6 +49,10 @@ export function Pour() {
       <button onClick={handleOnStart}>Start</button>
       <button onClick={handleOnPause}>Pause</button>
       <button onClick={handleOnReset}>Reset</button>
+      <p>step: {pacer.stepIndex}</p>
+      <p>step time: {pacer.stepCountdown}</p>
+      <p>total time: {pacer.totalTime}</p>
+      <p>total volume: {Math.floor(pacer.totalVolume.value)}</p>
     </section>
   );
 }
