3f5355fd8ce3b48c4e3d8d04c1fdaaf0bc0d7e56
Christian Albert <calbert1209@gmail.com>
2024-01-10 21:28:35 +0900

    add path base to config in production

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,22 @@ import preact from "@preact/preset-vite";
 import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [preact()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
+export default defineConfig(({ command }) => {
+  const baseConfig = {
+    plugins: [preact()],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
+
+  if (command === "serve") {
+    return baseConfig;
+  }
+
+  return {
+    ...baseConfig,
+    base: "/sabre-wing/",
+  };
 });
