f980542e423f1cf9a3620e9c6bd4d644fa05b87b
Christian Albert <calbert1209@gmail.com>
2024-01-15 00:46:01 +0900

    add qr plugin to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import { resolve } from "node:path";
+import { qrcode } from "vite-plugin-qrcode";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), qrcode()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
