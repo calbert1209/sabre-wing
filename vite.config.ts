55ae6aa6174e941a391e2534c47847674d49a7e0
Christian Albert <calbert1209@gmail.com>
2024-01-14 11:45:17 +0900

    use env var to set url base

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,12 @@ import preact from "@preact/preset-vite";
 import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const baseConfig = {
-    plugins: [preact()],
-    resolve: {
-      alias: {
-        "@": resolve(__dirname, "./src"),
-      },
+export default defineConfig({
+  plugins: [preact()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
     },
-  };
-
-  if (command === "serve") {
-    return baseConfig;
-  }
-
-  return {
-    ...baseConfig,
-    base: "/sabre-wing/",
-  };
+  },
+  base: `/${process.env.VITE_URL_BASE ?? ""}`,
 });
