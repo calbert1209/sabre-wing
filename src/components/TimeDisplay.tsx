778d8d5ff49a768f28e39068431ec712937ee42c
Christian Albert <calbert1209@gmail.com>
2024-01-14 23:35:00 +0900

    show step time in TimeDisplay

--- src/components/TimeDisplay.tsx
+++ src/components/TimeDisplay.tsx
@@ -1,24 +1,47 @@
 import "./timeDisplay.css";
 
-export function TimeDisplay({
+function TotalTimeDisplay({
   sec,
   running,
+  size,
 }: {
   sec: number;
   running?: boolean;
+  size?: "primary" | "secondary";
 }) {
   const m = Math.floor(sec / 60);
   const s = (sec % 60).toString(10).padStart(2, "0");
 
   return (
-    <div className="time-display" data-running={!!running}>
-      <div className="time-display-digits time-display-minutes">
+    <div
+      className="total-time-display"
+      data-running={!!running}
+      data-size={size ?? "secondary"}
+    >
+      <div className="total-time-display-digits total-time-display-minutes">
         <div>{m}</div>
       </div>
-      <div className="time-display-colon">{":"}</div>
-      <div className="time-display-digits time-display-seconds">
+      <div className="total-time-display-colon">{":"}</div>
+      <div className="total-time-display-digits time-display-seconds">
         <div>{s}</div>
       </div>
+    </div>
+  );
+}
+
+export function TimeDisplay({
+  sec,
+  stepTime,
+  running,
+}: {
+  sec: number;
+  stepTime: number;
+  running?: boolean;
+}) {
+  return (
+    <div className="time-display">
+      <TotalTimeDisplay sec={stepTime} size="secondary" />
+      <TotalTimeDisplay sec={sec} running={running} size="primary" />
     </div>
   );
 }
