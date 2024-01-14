6f1024884ea567211b707ac8bcf44b6c874abcca
Christian Albert <calbert1209@gmail.com>
2024-01-14 11:45:32 +0900

    use url_base to build routes

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,4 +1,8 @@
+const baseUrl = import.meta.env.BASE_URL;
+// In dev environment BASE_URL is "/", prod it's set to "/sabre-wing"
+const base = baseUrl.length > 1 ? baseUrl : "";
+
 export const Routes = {
-  pour: "/pour",
-  root: "/",
+  pour: `${base}/pour`,
+  root: `${base}/`,
 } as const;
