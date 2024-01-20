d138d0e3285cb87162e67a193730b26de09684c0
Christian Albert <calbert1209@gmail.com>
2024-01-20 21:56:38 +0900

    :broom: remove unused Route constants

--- src/constants/routes.ts
+++
@@ -1,8 +0,0 @@
-const baseUrl = import.meta.env.BASE_URL;
-// In dev environment BASE_URL is "/", prod it's set to "/sabre-wing"
-const base = baseUrl.length > 1 ? baseUrl : "";
-
-export const Routes = {
-  pour: `${base}/pour`,
-  root: `${base}/`,
-} as const;
