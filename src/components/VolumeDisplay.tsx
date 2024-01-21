97308bd0f4745173d78a9bc9ab2284a1b420d99c
Christian Albert <calbert1209@gmail.com>
2024-01-21 16:40:38 +0900

    add progress ramp to volume display

--- src/components/VolumeDisplay.tsx
+++ src/components/VolumeDisplay.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "preact/hooks";
 import "./volumeDisplay.css";
 
 export function VolumeDisplay(props: {
@@ -11,17 +12,31 @@ export function VolumeDisplay(props: {
     Math.ceil(props.end),
   ];
 
+  const percent = useMemo(() => {
+    const dVol = props.totalVolume - props.start;
+    if (dVol <= 0) return 0;
+
+    return (dVol / (props.end - props.start)) * 100;
+  }, [props.totalVolume, props.start, props.end]);
+
   return (
-    <div className="volume-display">
-      <div className="volume-display-number volume-display-secondary">
-        {start}
+    <>
+      <div className="volume-display">
+        <div className="volume-display-number volume-display-primary">
+          <div>{totalVolume}</div>
+        </div>
+        <div className="volume-display-number volume-display-secondary volume-display-end">
+          {end}
+        </div>
       </div>
-      <div className="volume-display-number volume-display-primary">
-        <div>{totalVolume}</div>
+      <div className="volume-display__ramp-row">
+        <div className="volume-display__ramp-container">
+          <div
+            className="volume-display__progress-indicator"
+            style={{ width: `${percent}%` }}
+          />
+        </div>
       </div>
-      <div className="volume-display-number volume-display-secondary volume-display-end">
-        {end}
-      </div>
-    </div>
+    </>
   );
 }
